c5f260fc56b22159d5a28117a346e1b3e7183e2c
frogman21-ai <frogm1708@gmail.com>
2026-06-28 14:34:30 +0700

    nilai tidak valid

---
+++ lulus2.py
@@ -0,0 +1,10 @@
+nilai = int(input("nilai kamu: "))
+
+if nilai <=100:
+    if nilai >= 75:
+        print("lulus")
+    else:
+        print("tidak lulus")
+
+else:
+    print("nilai tidak valid ")
